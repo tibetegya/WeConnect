3c8ce7b9a50d307c09fc8306dbacbe3c13c1c19a
george tibetegya <gtibetegya@gmail.com>
2018-02-20 23:26:20 +0300

    move files to api-application folder

---
+++ api-application/app/apis.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from app import app, api
+
+todos = {}
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
