daadd46d7d9fc7e3e4b647d17946ab5d30db3c87
george tibetegya <gtibetegya@gmail.com>
2018-02-21 06:39:51 +0300

    add business api

--- api-application/app/apis.py
+++ api-application/app/apis.py
@@ -1,14 +1,67 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import reqparse, abort, Api, Resource
 from app import app, api
 
-todos = {}
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+businesses = {
+    'business1':{
+        'id': 1,
+        'name': 'Business 1',
+        'location': 'kampala',
+        'profile': 'picture.png',
+        'creation_date': '21 Febuary 2018',
+        'business_owner': 'george'        
+        },
+    'business2':{
+        'id': 2,
+        'name': 'Business 2',
+        'location': 'mukono',
+        'profile': 'picture.png',
+        'creation_date': '21 Febuary 2018',
+        'business_owner': 'george'        
+        },
+}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+parser = reqparse.RequestParser()
+parser.add_argument('business')
+
+class Business(Resource):
+    def get(self, business_id):
+        return businesses[business_id]
+        
+    def delete(self, business_id):
+        del businesses[business_id]
+        return '', 204
+    def put(self, business_id):
+        args = parser.parse_args()
+        business = {
+                    'id': args['id'] ,
+                    'name': args['name'],
+                    'location': args['location'],
+                    'profile': args['profile'],
+                    'creation_date': args['date'],
+                    'business_owner': args['business_owner']        
+                    }
+        businesses[business_id] = business
+        return business, 201
+
+class BusinessList(Resource):
+    def get(self):
+        return businesses
+
+    def post(self):
+        args = parser.parse_args()
+        business_id = int(max(businesses.keys()).lstrip('business')) + 1
+        business_id = 'business%i' % business_id
+        businesses[business_id] = {
+                    'id': args['id'] ,
+                    'name': args['name'],
+                    'location': args['location'],
+                    'profile': args['profile'],
+                    'creation_date': args['date'],
+                    'business_owner': args['business_owner']        
+                    }
+        return businesses[business_id], 201
+
+api.add_resource(BusinessList, '/businesses')
+api.add_resource(Business, '/businesses/<business_id>')
