027d305a04a0d696714986550fd186f7a1c81abf
george tibetegya <gtibetegya@gmail.com>
2018-02-21 15:21:05 +0300

    users api

--- api-application/app/__init__.py
+++ api-application/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
+from config import Config
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
 
-from app import business_api
+from app import user_api
