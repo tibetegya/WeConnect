c36e70c663f1c40dbdf299fbbeb3439f93f46f8e
tibetegya <gtibetegya@gmail.com>
2018-03-19 15:45:09 +0300

    add manage.py

---
+++ manage.py
@@ -0,0 +1,17 @@
+import unittest
+from flask_script import Manager
+from app import app
+
+manager = Manager(app)
+
+@manager.command
+def test():
+    tests = unittest.TestLoader().discover('./v1/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+
+    # if result.wasSuccessful() return 0 else 1
+    # return the opposite boolean
+    return not result.wasSuccessful()
+
+if __name__ == '__main__':
+    manager.run()
