793be0ee22711bb5aed5e48d017c7ff1b8ba0c65
George Tibetegya <gtibetegya@gmail.com>
2018-04-15 12:50:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-import os
 from flask import Flask  
-from config import Config
-from v1 import blueprint as api_v1
-
-
-app = Flask(__name__)
-
-app.config.from_object(Config)
-app.config['SWAGGER_UI_JSONEDITOR']= True
-app.config['SECRET_KEY'] = 'george is awesome'
-
-
-app.register_blueprint(api_v1, url_prefix='/api/v1')
-
+from v1 import app
 
 if __name__ == '__main__':
     app.run(debug=True)
